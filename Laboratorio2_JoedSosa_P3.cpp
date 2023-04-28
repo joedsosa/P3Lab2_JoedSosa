3ccfc88b86919e1444749a52be543528d51dce77
Joed Sosa <joedsosa5@gmail.com>
2023-04-28 15:19:24 -0600

    segundo ejercico creado

--- Laboratorio2_JoedSosa_P3.cpp
+++ Laboratorio2_JoedSosa_P3.cpp
@@ -1,7 +1,10 @@
 
-
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <array>
 using namespace std;
+const int tam = 10;
 int fact(int n) {
 	if (n == 1 || n == 0) {
 		return 1;
@@ -18,20 +21,72 @@ void crearTriangulo(int n) {
 		cout << endl;
 	}
 }
-int main() {
-	int num;
-	cout << "Ingrese un entero positivio: ";
-	cin >> num;
-	while (num > 0) {
-		cout << "El numero debe ser positivo. " << endl;
-		cout << "Ingrese un entero positivio: ";
-		cin >> num;
+void printArray(char arr[]) {
+	for (int i = 0; i < tam; i++){
+		cout << arr[i] << " ";
 	}
-	int f = fact(num);
-	cout << "El factorial de " << num << "es: " << f << endl;
-	cout << "Triangulo de: " << f << "*" << endl;
-	crearTriangulo(f);
-
 }
+void orderArray(char arr[], int tam) {
+	for (int i = 0; i < tam - 1; i++){
+		for (int j = i + 1; j < tam; j++){
+			if (arr[i] > arr[j]) {
+				char temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
+int main() {
+	srand(time(0));
+	char opcion;
+	bool continuar = true;
+	char arr[10];
+	do {
+		cout << "MENU\n";
+		cout << "1. Calcular factorial\n";
+		cout << "2. Ordenar arreglo \n";
+		cout << "3. Salir\n";
+		cout << "Seleccione una opcion: ";
+		cin >> opcion;
+		switch (opcion) {
+		case '1':
+			int n;
+			cout << "Ingrese un entero positivo: ";
+			cin >> n;
+			cout << "El factorial de" << n << " es: " << fact(n) << endl;
+			crearTriangulo(n);
+			break;
+		case '2':
+			for (int i = 0; i < tam; i++) {
+				arr[i] = 'a' + rand() % 26;
+			}
+			cout << "Random Array generado";
+			for (int i = 0; i < tam; i++) {
+				cout << arr[i] << " ";
+			}
+			cout << endl;
+			orderArray(arr, tam);
+			cout << "Arreglo ordenado sin reps";
+			for (int i = 0; i < tam; i++) {
+				if (arr[i] != '\0') {
+					cout << arr[i] << " ";
+				}
+			}
+			cout << endl;
+			break;
+		case '3':
+			cout << "Saliendo del programa" << endl;
+			break;
+		default :
+			cout << "Opcion ivalida" << endl;
+		}
+		if (opcion != '3') {
+			cout << "Desea continuar ? (S / N) :";
+			cin >> opcion;
+		}
 
-
+		} while (opcion == 'S' || opcion == 's');
+		return 0;
+	
+}
